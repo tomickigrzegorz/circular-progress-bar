fa6c21d7c66c4fed965c75dc4da6d339ebdde805
Grzegorz Tomicki <gtomicki@gmail.com>
2019-06-25 22:41:53 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,17 +1,90 @@
-const stroke = document.querySelector('#stroke');
-const percentNumber = document.querySelector('.percent');
-const percent = 65;
-const end = 264;
+class CircularProgressBar {
+  constructor(options) {
+    this.options = options;
 
-function circularProgressBar() {
-  for (let i = 0; i <= end; i++) {
-    setTimeout(() => {
-      if(i > percent) return;
-      percentNumber.innerHTML = `${i}%`;
-      let d = parseInt(i * 2.64);
-      stroke.setAttribute('stroke-dasharray', `${d} ${end - d}`);
-    }, i * 30);
+    const { pie, percent, color, strokeWidth, opacity, number, size, fontSize, fontWeight, fontColor } = this.options;
+    this.pie = document.querySelector(`.${pie}`);
+    this.percent = percent || 65;
+    this.color = color || '#00a1ff';
+    this.strokeWidth = strokeWidth || 10;
+    this.opacity = opacity || 0.1;
+    this.number = number || true;
+    this.size = size || 200;
+    this.fontSize = fontSize || '3rem';
+    this.fontWeight = fontWeight || 700;
+    this.fontColor = fontColor || '#365b74';
+    this.end = 264;
+
+    this.createSvg();
   }
+
+  hexTorgb(fullhex) {
+    const hex = fullhex.substring(1, 7);
+    const rgb = `${parseInt(hex.substring(0, 2), 16)}, ${parseInt(hex.substring(2, 4), 16)},${parseInt(hex.substring(4, 6), 16)}`;
+    return rgb;
+  }
+
+  circularProgressBar() {
+    const stroke = document.querySelector('#stroke');
+    const size = this.size.toString().substring(0, 1);
+    const sizeStroke = this.size < 100 ? size / 10 : size;
+
+    console.log(sizeStroke);
+
+    this.percentElement();
+
+    for (let i = 0; i <= this.end; i++) {
+      setTimeout(() => {
+        if (i > this.percent) return;
+        if (this.number) {
+          this.percentElementUpdate(i)
+        }
+        let d = parseInt(i * 2.64);
+        stroke.setAttribute('style', `fill: transparent; stroke: ${this.color}; stroke-width: ${this.strokeWidth}; stroke-dashoffset: 66; stroke-dasharray: ${d} ${this.end - d}`);
+      }, i * 30);
+    }
+
+    this.pie.setAttribute('style', `box-shadow: inset 0px 0px ${this.strokeWidth}px ${this.strokeWidth}px rgba(${this.hexTorgb(this.color)}, ${this.opacity})`);
+
+  }
+
+  percentElement() {
+    const percent = document.createElement('div');
+    percent.className = 'percent'
+    percent.setAttribute('style', `font-size: ${this.fontSize}; font-weight: ${this.fontWeight}; color: ${this.fontColor}`);
+    this.pie.appendChild(percent);
+  }
+
+  percentElementUpdate(numbers) {
+    const percentNumber = document.querySelector('.percent');
+    percentNumber.innerHTML = `${numbers}%`;
+  }
+
+
+  createSvg() {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+
+    svg.setAttribute('width', this.size);
+    svg.setAttribute('height', this.size);
+    svg.setAttribute('viewBox', '0 0 100 100');
+    svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+
+    circle.setAttribute('cx', 50);
+    circle.setAttribute('cy', 50);
+    circle.setAttribute('r', 42);
+    circle.setAttribute('id', 'stroke');
+
+    svg.appendChild(circle);
+    this.pie.appendChild(svg)
+
+    this.circularProgressBar()
+  }
+
 }
 
-window.addEventListener('DOMContentLoaded', circularProgressBar());
+window.addEventListener('DOMContentLoaded', () => {
+  new CircularProgressBar({
+    pie: 'pie'
+  });
+});
