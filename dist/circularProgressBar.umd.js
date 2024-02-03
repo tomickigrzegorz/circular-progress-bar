83dca4aa5757a4027fb32d681b57b580953dcd8c
Grzegorz Tomicki <gtomik@gmail.com>
2024-02-04 00:48:53 +0100

    fix: Inverse option is not working in ios [#87]

--- dist/circularProgressBar.umd.js
+++ dist/circularProgressBar.umd.js
@@ -67,7 +67,7 @@
   const dashOffset = (count, inverse, cut) => {
     const cutChar = cut ? 264 / 100 * (100 - cut) : 264;
     const angle = 264 - count / 100 * cutChar;
-    return inverse ? -angle : angle;
+    return inverse ? `${264 - angle}` : angle;
   };
   const insertAdElement = (element, el, type = "beforeend") => element.insertAdjacentElement(type, el);
   const gradient = ({
